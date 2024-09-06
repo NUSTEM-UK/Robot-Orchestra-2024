6abad4ae03ea26ebca5f5be01a61ff754e9d7ebd
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2024-09-06 17:27:31 +0100

    Trellis animation order.

--- experiments/trellis-test03.py
+++ experiments/trellis-test03.py
@@ -7,12 +7,34 @@ i2c = busio.I2C(SCL, SDA)
 
 matrix0 = Trellis(i2c, [0x70])
 matrix1 = Trellis(i2c, [0x71])
+matrix2 = Trellis(i2c, [0x72])
+matrix3 = Trellis(i2c, [0x73])
+matrix4 = Trellis(i2c, [0x74])
+matrix5 = Trellis(i2c, [0x75])
+matrix6 = Trellis(i2c, [0x76])
+matrix7 = Trellis(i2c, [0x77])
 
-matrix0.led.fill(True)
-time.sleep(1)
-matrix1.led.fill(True)
-time.sleep(1)
-matrix0.led.fill(False)
-time.sleep(1)
-matrix1.led.fill(False)
+trellis_set = [matrix0, matrix1, matrix2, matrix3, matrix7, matrix6, matrix5, matrix4]
+
+
+for _ in range(10):
+
+    for trellis in trellis_set:
+        trellis.led.fill(True)
+        time.sleep(0.03)
+
+    # time.sleep(1)
+
+    for trellis in trellis_set:
+        trellis.led.fill(False)
+        time.sleep(0.03)
+
+
+# matrix0.led.fill(True)
+# time.sleep(1)
+# matrix1.led.fill(True)
+# time.sleep(1)
+# matrix0.led.fill(False)
+# time.sleep(1)
+# matrix1.led.fill(False)
 
